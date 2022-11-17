2d5fffc808d5d2d3b74e9d93734604bf5693c52f
jaeYoun <example@example.com>
2022-11-18 01:20:32 +0900

    [feat: camp 1-1-9]

---
+++ app/src/main/java/com/codelab/basics/ui/theme/Theme.kt
@@ -0,0 +1,46 @@
+package com.codelab.basics.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorPalette = darkColorScheme(
+    primary = Purple200,
+    onPrimary = Purple700,
+    secondary = Teal200
+)
+
+private val LightColorPalette = lightColorScheme(
+    primary = Color.Blue,
+    onPrimary = Color.White,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+@Composable
+fun BasicsCodelabTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        shapes = Shapes,
+        content = content
+    )
+}
